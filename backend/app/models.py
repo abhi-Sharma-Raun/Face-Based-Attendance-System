861ff365171bf67d0e100814f5ec6d05a45c5118
abhi-Sharma-Raun <abhinandantyagiraun@gmail.com>
2025-10-04 00:32:48 +0530

    code update

--- backend/app/models.py
+++ backend/app/models.py
@@ -7,8 +7,8 @@ from sqlalchemy.sql.expression import text
 class Users(Base):
     __tablename__="users_Teachers"
     user_id=Column(String(20), primary_key=True)
-    email=Column(String, unique=True, nullable=False)
-    password_hash=Column(String,nullable=False)  #do authentication logic after setting up registration pipeline      
+    email=Column(String, ForeignKey("verified_emails.email_id", ondelete="CASCADE"), unique=True, nullable=False)
+    password_hash=Column(String,nullable=False)       
     created_at=Column(TIMESTAMP(timezone=True), server_default=text('now()'), nullable=False)
 
 class Students(Base):
@@ -16,7 +16,7 @@ class Students(Base):
     student_id=Column(UUID(as_uuid=True), primary_key=True, server_default=text("gen_random_uuid()"))
     roll_no=Column(String, nullable=False) 
     name=Column(String, nullable=False)
-    email=Column(String, nullable=True)  #keep it optional for now make it unique and compulsory later make the teacher_id+email unique
+    email=Column(String, nullable=True)  
     mime_type=Column(String(50), nullable=True)
     face_encoding=Column(LargeBinary, nullable=False)
     teacher_id=Column(String(20), ForeignKey("users_Teachers.user_id", ondelete="CASCADE"), nullable=False)
